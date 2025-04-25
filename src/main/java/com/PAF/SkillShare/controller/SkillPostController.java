711d86f841fc4209167349dd53ea2e6bf37e8ae0
Dumindu Mendis <121997251+Beliver-247@users.noreply.github.com>
2025-04-25 18:47:14 +0530

    Initial commit

---
+++ src/main/java/com/PAF/SkillShare/controller/SkillPostController.java
@@ -0,0 +1,25 @@
+package com.PAF.SkillShare.controller;
+
+import com.PAF.SkillShare.model.SkillPost;
+import com.PAF.SkillShare.service.SkillPostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/posts")
+public class SkillPostController {
+    @Autowired
+    private SkillPostService service;
+
+    @GetMapping
+    public List<SkillPost> getAll() { return service.getAll(); }
+    @GetMapping("/{id}") public SkillPost get(@PathVariable String id) { return service.getById(id); }
+    @PostMapping public SkillPost create(@RequestBody SkillPost post) { return service.create(post); }
+    @PutMapping("/{id}") public SkillPost update(@PathVariable String id, @RequestBody SkillPost post) {
+        return service.update(id, post);
+    }
+    @DeleteMapping("/{id}") public void delete(@PathVariable String id) { service.delete(id); }
+}
+
